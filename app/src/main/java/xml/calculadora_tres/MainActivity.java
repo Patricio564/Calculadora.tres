198f99bb49b908d93d0e5184ec70366d516e0073
Virna <v.ortega12@alumnos.santotomas.cl>
2022-10-16 01:22:12 -0300

    modificacion

--- app/src/main/java/xml/calculadora_tres/MainActivity.java
+++ app/src/main/java/xml/calculadora_tres/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-private Button button_enviar;
+private Button button_calcular;
 private TextView text_respuesta;
+private TextView textView_x;
 
 private EditText editText_n1;
 private EditText editText_n2;
@@ -21,19 +22,20 @@ private EditText edittext_n3;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_enviar=findViewById(R.id.button_enviar);
-        button_enviar.setOnClickListener(new View.OnClickListener() {
+        button_calcular=findViewById(R.id.button_calcular);
+        button_calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text_respuesta.setText(enviar(Integer.parseInt(editText_n1.getText().toString()),Integer.parseInt(editText_n2.getText().toString()),Integer.parseInt(edittext_n3.getText().toString()))+" ");
+                text_respuesta.setText(calcular(Double.parseDouble(editText_n1.getText().toString()),Double.parseDouble(editText_n2.getText().toString()),Double.parseDouble(edittext_n3.getText().toString()))+" ");
             }
         });
         text_respuesta=findViewById(R.id.text_respuesta);
     }
-    public double enviar (int a,int b,int c){
-        int respuesta =0;
+    public float calcular(Double a,Double b,Double c){
+        float respuesta =0;
+
         if (a!=0){
-            respuesta =(c*b)/a;
+            respuesta = (float)((a*b)/c);
         }
         return respuesta;
     }
