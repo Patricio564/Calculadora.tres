4a200576fb6ca651945dbdd4f07840b31800f379
Virna <v.ortega12@alumnos.santotomas.cl>
2022-10-21 00:35:14 -0300

    modificacion

--- app/src/main/java/xml/calculadora_tres/MainActivity.java
+++ app/src/main/java/xml/calculadora_tres/MainActivity.java
@@ -10,33 +10,42 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-private Button button_calcular;
-private TextView text_respuesta;
-private TextView textView_x;
+    private Button button_calcular;
+    private TextView text_respuesta;
 
-private EditText editText_n1;
-private EditText editText_n2;
-private EditText edittext_n3;
+    private EditText num1;
+    private EditText num2;
+    private EditText porcentaje1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        porcentaje1 = findViewById(R.id.porcentaje1);
         button_calcular=findViewById(R.id.button_calcular);
+        text_respuesta=findViewById(R.id.text_respuesta);
+
         button_calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text_respuesta.setText(calcular(Double.parseDouble(editText_n1.getText().toString()),Double.parseDouble(editText_n2.getText().toString()),Double.parseDouble(edittext_n3.getText().toString()))+" ");
+                text_respuesta.setText(calcular(Double.parseDouble(num1.getText().toString()),Double.parseDouble(num2.getText().toString()),Double.parseDouble(porcentaje1.getText().toString()))+" ");
             }
         });
-        text_respuesta=findViewById(R.id.text_respuesta);
-    }
-    public float calcular(Double a,Double b,Double c){
-        float respuesta =0;
 
-        if (a!=0){
-            respuesta = (float)((a*b)/c);
-        }
-        return respuesta;
+
+    }
+
+
+
+
+    public double calcular(Double numero_1 ,Double numero_2 ,Double porcentaje_1){
+
+
+
+        return ((numero_2 * porcentaje_1)/numero_1);
     }
 }
